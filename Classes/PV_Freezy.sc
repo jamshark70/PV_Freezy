5106353cd62c1311cc69e88a185ad5ef08f30ab1
James Harkins <jamshark70@zoho.com>
2024-12-06 10:53:24 +0800

    Remove PV_Lag: All can be implemented in PV_Freezish

--- Classes/PV_Freezy.sc
+++ Classes/PV_Freezy.sc
@@ -1,14 +1,23 @@
 PV_Freezish : PV_ChainUGen {
-	*new { |buffer, freeze = 0|
-		^this.multiNew('control', buffer, freeze)
+	*new { |buffer, atkCoeff = 0, dcyCoeff = 0|
+		^this.multiNew('control', buffer, atkCoeff, dcyCoeff)
 	}
-}
 
-PV_Lag : PV_ChainUGen {
-	*new { |buffer, attack = 0.01, decay = 1, hop = 0.5|
-		^this.multiNew('control', buffer,
-			attack, decay,
-			SampleRate.ir / BufFrames.kr(buffer) / hop
-		)
+	// exp(log001 / numDecay)
+	// numDecay = time * (sr / framesize / hop)
+	// exp(log001 / (time * sr / framesize / hop)))
+	// exp(log001 / time * sd * framesize * hop))
+	*lag { |buffer, atkTime = 0.01, dcyTime = 0.01, hop = 0.5|
+		var sdur = SampleDur.ir;
+		var frames = if(buffer.rate == \control) {
+			Latch.kr(BufFrames.kr(buffer), buffer >= 0)
+		} {
+			buffer.numFrames
+		};
+		var factor = log(0.001) * hop * frames * sdur;
+		var coeff = { |time|
+			exp(factor / max(0.001, time))
+		};
+		^this.multiNew('control', buffer, coeff.(atkTime), coeff.(dcyTime))
 	}
 }
--- source/PVFreezy.cpp
+++ source/PVFreezy.cpp
@@ -50,6 +50,10 @@ float phaseminus(float phase1, float phase2) {
   return dif;
 }
 
+float interpolateUpDown(float in, float fb, float atkCoeff, float dcyCoeff) {
+	return (fb - in) * ((in > fb) ? atkCoeff : dcyCoeff) + in;
+}
+
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
@@ -62,26 +66,12 @@ struct PV_Freezish : public PV_Unit {
 	int m_stage;
 };
 
-struct PV_Lag : PV_Unit {
-	int m_numbins;
-	float *m_mags, m_dc, m_nyq;
-	float *m_prevPhases, *m_difPhases;
-	SndBuf *m_buf;
-	int m_stage;
-	float m_atkTime, m_atkCoeff, m_dcyTime, m_dcyCoeff;
-	// float m_frameIOI;
-};
-
 extern "C" {
 	#include "SC_fftlib.h"
 
 	void PV_Freezish_Ctor(PV_Freezish *unit);
 	void PV_Freezish_next(PV_Freezish* unit, int inNumSamples);
 	void PV_Freezish_Dtor(PV_Freezish *unit);
-
-	void PV_Lag_Ctor(PV_Lag *unit);
-	void PV_Lag_next(PV_Lag* unit, int inNumSamples);
-	void PV_Lag_Dtor(PV_Lag *unit);
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
@@ -105,7 +95,8 @@ void PV_Freezish_next(PV_Freezish *unit, int inNumSamples)
 	float *prevPhases = unit->m_prevPhases;
 	SCPolarBuf *p = ToPolarApx(buf);
 	float *difPhases = unit->m_difPhases;
-	float freeze = ZIN0(1);
+	float atkCoeff = ZIN0(1);
+	float dcyCoeff = ZIN0(2);
 
 	switch(unit->m_stage) {
 	case 0:
@@ -135,7 +126,7 @@ void PV_Freezish_next(PV_Freezish *unit, int inNumSamples)
 		for (int i=0; i<numbins; ++i) {
 		  float in = p->bin[i].mag;
 		  float fb = mags[i];
-		  float out = (fb - in) * freeze + in;
+		  float out = interpolateUpDown(in, fb, atkCoeff, dcyCoeff);
 		  p->bin[i].mag = out;
 		  mags[i] = out;
 
@@ -149,12 +140,11 @@ void PV_Freezish_next(PV_Freezish *unit, int inNumSamples)
 	case 3:
 		// normal case
 		// freeze previous magnitudes
-	  // float freezeMul = ZIN0(2);
-	  float distFreeze = freeze; // std::tanh((freeze * 2.f - 1.f) * freezeMul) * 0.5f + 0.5f;
 		for (int i=0; i<numbins; ++i) {
 		  float in = p->bin[i].mag;
 		  float fb = mags[i];
-		  float out = (fb - in) * freeze + in;
+		  float coeff = (in > fb) ? atkCoeff : dcyCoeff;
+		  float out = interpolateUpDown(in, fb, atkCoeff, dcyCoeff);
 		  p->bin[i].mag = out;
 		  mags[i] = out;
 
@@ -163,7 +153,9 @@ void PV_Freezish_next(PV_Freezish *unit, int inNumSamples)
 		  // I guess perhaps that difPhases decays toward 0
 		  float difPhaseIn = phaseminus(p->bin[i].phase, prevPhases[i]);
 		  float difPhaseFb = difPhases[i];
-		  difPhases[i] = (difPhaseFb - difPhaseIn) * distFreeze + difPhaseIn;
+		  // interpolateUpDown() can't be used b/c 'mag' comparison is relevant, not phase
+
+		  difPhases[i] = (difPhaseFb - difPhaseIn) * coeff + difPhaseIn;
 		  float phase = prevPhases[i] + difPhases[i];
 		  // if(i == 5) printf("difPhases[5] = %f\n", difPhases[i]);
 		  
@@ -174,8 +166,8 @@ void PV_Freezish_next(PV_Freezish *unit, int inNumSamples)
 		  p->bin[i].phase = phase;
 		  prevPhases[i] = phase;
 		}
-		p->dc = (unit->m_dc - p->dc) * freeze + p->dc;
-		p->nyq = (unit->m_nyq - p->nyq) * freeze + p->nyq;
+		unit->m_dc = p->dc = interpolateUpDown(p->dc, unit->m_dc, atkCoeff, dcyCoeff);
+		unit->m_nyq = p->nyq = interpolateUpDown(p->nyq, unit->m_nyq, atkCoeff, dcyCoeff);
 		break;
 	}
 }
@@ -197,118 +189,6 @@ void PV_Freezish_Dtor(PV_Freezish* unit)
 
 
 
-// PV_Lag: up and down coefficients
-// PV_Lag(buffer, atkTime, relTime, framesPerSec)
-
-float interpolateUpDown(float in, float fb, float atkCoeff, float dcyCoeff) {
-	return (fb - in) * ((in > fb) ? atkCoeff : dcyCoeff) + in;
-}
-
-void PV_Lag_next(PV_Lag *unit, int inNumSamples)
-{
-	PV_GET_BUF
-
-	float *mags = unit->m_mags;
-	float *prevPhases = unit->m_prevPhases;
-	SCPolarBuf *p = ToPolarApx(buf);
-	float *difPhases = unit->m_difPhases;
-
-	float fps = ZIN0(3);
-	float atk = ZIN0(1);
-	float atkCoeff = unit->m_atkCoeff;
-	if(atk != unit->m_atkTime) {
-		unit->m_atkTime = atk;
-		unit->m_atkCoeff = atkCoeff = exp(log001 / (atk * fps));
-	}
-	float dcy = ZIN0(2);
-	float dcyCoeff = unit->m_dcyCoeff;
-	if(dcy != unit->m_dcyTime) {
-		unit->m_dcyTime = dcy;
-		unit->m_dcyCoeff = dcyCoeff = exp(log001 / (dcy * fps));
-	}
-
-	switch(unit->m_stage) {
-	case 0:
-		// no FFT frames are ready yet. Only allocate storage
-		unit->m_mags = (float*)RTAlloc(unit->mWorld, numbins * sizeof(float));
-		unit->m_difPhases = (float*)RTAlloc(unit->mWorld, numbins * sizeof(float));
-		unit->m_prevPhases = (float*)RTAlloc(unit->mWorld, numbins * sizeof(float));
-		unit->m_numbins = numbins;
-		unit->m_stage = 1;
-		break;
-	case 1:
-		// exactly one FFT frame is ready. Save its mags and phases. output = input
-		for (int i=0; i<numbins; ++i) {
-			mags[i] = p->bin[i].mag;
-			prevPhases[i] = p->bin[i].phase;
-		}
-		unit->m_dc = p->dc;
-		unit->m_nyq = p->nyq;
-		unit->m_stage = 2;
-		break;
-	case 2:
-		for (int i=0; i<numbins; ++i) {
-		  float in = p->bin[i].mag;
-		  float fb = mags[i];
-		  float out = interpolateUpDown(in, fb, atkCoeff, dcyCoeff);
-		  p->bin[i].mag = out;
-		  mags[i] = out;
-
-		  difPhases[i] = phaseminus(p->bin[i].phase, prevPhases[i]);
-		  prevPhases[i] = p->bin[i].phase;
-		}	      
-		p->dc = unit->m_dc;
-		p->nyq = unit->m_nyq;
-		unit->m_stage = 3;
-		break;
-	case 3:
-		// normal case
-		for (int i=0; i<numbins; ++i) {
-		  float in = p->bin[i].mag;
-		  float fb = mags[i];
-		  float binCoeff = (in > fb) ? atkCoeff : dcyCoeff;
-		  float out = (fb - in) * binCoeff + in;
-		  p->bin[i].mag = out;
-		  mags[i] = out;
-
-		  float difPhaseIn = phaseminus(p->bin[i].phase, prevPhases[i]);
-		  float difPhaseFb = difPhases[i];
-		  difPhases[i] = (difPhaseFb - difPhaseIn) * binCoeff + difPhaseIn;
-		  float phase = prevPhases[i] + difPhases[i];
-		  // if(i == 5) printf("difPhases[5] = %f\n", difPhases[i]);
-		  
-		  if (phase > pi) /* wrap the phase */
-		      phase -= twopi;
-		  if (phase < -pi)
-		      phase += twopi;
-		  p->bin[i].phase = phase;
-		  prevPhases[i] = phase;
-		}
-		// p->dc = (unit->m_dc - p->dc) * freeze + p->dc;
-		// p->nyq = (unit->m_nyq - p->nyq) * freeze + p->nyq;
-		unit->m_dc = p->dc = interpolateUpDown(p->dc, unit->m_dc, atkCoeff, dcyCoeff);
-		unit->m_nyq = p->nyq = interpolateUpDown(p->nyq, unit->m_nyq, atkCoeff, dcyCoeff);
-		break;
-	}
-}
-
-
-void PV_Lag_Ctor(PV_Lag* unit)
-{
-	SETCALC(PV_Lag_next);
-	ZOUT0(0) = ZIN0(0);
-	unit->m_stage = 0;
-}
-
-void PV_Lag_Dtor(PV_Lag* unit)
-{
-	RTFree(unit->mWorld, unit->m_mags);
-	RTFree(unit->mWorld, unit->m_difPhases);
-	RTFree(unit->mWorld, unit->m_prevPhases);
-}
-
-
-
 void init_SCComplex(InterfaceTable *inTable);
 
 #define DefinePVUnit(name) (*ft->fDefineUnit)(#name, sizeof(PV_Unit), (UnitCtorFunc)&name##_Ctor, 0, 0);
@@ -321,6 +201,5 @@ PluginLoad(PV_Freezy)
     init_SCComplex(inTable);
 
     DefineDtorUnit(PV_Freezish);
-    DefineDtorUnit(PV_Lag);
 }
 
